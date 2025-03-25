292887e688c98f91ee4b7270d14a6f9801452a03
Areo-Joe <1047726015@qq.com>
2025-03-25 23:50:31 +0800

    feat: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default App;
